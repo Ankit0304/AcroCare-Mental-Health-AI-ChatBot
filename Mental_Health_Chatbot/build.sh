55e66da2aef01a2d13c3c315066a0d875ada8fb3
Ankit <ankitbaingane157@gmail.com>
2026-09-23 12:58:27 +0530

    Fix profile picture display: enable production media serving in urls.py, add default image, and fallback handlers

--- Mental_Health_Chatbot/build.sh
+++ Mental_Health_Chatbot/build.sh
@@ -4,5 +4,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
+mkdir -p media/profile_pics
+if [ ! -f media/default.jpg ] && [ -f static/images/account.png ]; then
+    cp static/images/account.png media/default.jpg
+fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
